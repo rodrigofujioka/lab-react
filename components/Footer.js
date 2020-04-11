3b6466279459b2a972f2e0e7505a2f422ea25fa5
Rodrigo Fujioka <rodrigofujioka@gmail.com>
2020-04-11 04:30:34 -0300

    fullStackLab lab

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='py-4 text-center bg-gray-400'>
+            MyDailyStatus é um projeto criado durante o Fullstack Lab do DevPleno.<br />
+            Implementado por: <a href='https://www.linkedin.com/in/guilherme-antonio-ferrari/' target='_blank'>Guilherme Ferrari</a>
+        </div>
+    )
+}
+
+export default Footer
